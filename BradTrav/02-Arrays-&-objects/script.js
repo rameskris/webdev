be2e81f3613e41e4fd3d3a89ba001a66c553d088
rameskris <redmiblack2@gmail.com>
2023-03-18 20:41:59 +0530

    secondSection relocate

---
+++ BradTrav/02-Arrays-&-objects/script.js
@@ -0,0 +1,14 @@
+//Array literal
+const number=[12,34,45]
+
+//array constructor
+const fruits=new Array('apple','orange','grape')
+
+let x;
+x=number.length
+fruits[2]='pear'
+
+
+fruits.length=2;
+x=fruits
+console.log(x)
